ad84fdc4e3ab3030d62813263c1b4a274e131ee3
Raza403 <raza403@gmail.com>
2018-12-25 22:06:01 +0500

    Started learning php, some basic learned today

---
+++ learning/First/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>First PHP site</title>
+</head>
+
+<body>
+
+  <?php
+    // Echoing in PHP
+    // echo "<h1>Hello</h1>";
+    // echo "<hr>";
+    // echo "<p>This is my site</p>";
+
+    // // Use of variables
+    // $name = "Ahmed";
+    // $age = 28;
+    // echo "There once was a man named $name <br>";
+    // echo "He was $age years old<br>";
+    // // Update variable 
+    // $name = "Hamid";
+    // $age = 23;
+    // echo "He really like the name $name<br> But he didn't like to be $age years old";
+
+    // // Data Types in PHP
+    $phrase = "Today is 25 dec<br>";
+    // $age = 35;
+    // $negative_value = -23;
+    // $float_int = 3.1415;
+    // $bool = true;
+    // echo $phrase;
+    // echo strtolower($phrase);
+    // echo strtoupper($phrase);
+    // echo strtoupper("dogs<br>");
+    // echo $phrase[0];
+    // $phrase[0] = "t";
+    // echo "<br>";
+    // echo $phrase[0];
+    // echo "<br>";
+    // echo strlen("Hello");
+    // echo "<br>";
+    // echo str_replace("today", "Yesterday", $phrase);
+    // echo str_replace("to", "Yester", $phrase);
+    // echo $phrase;
+    // // Just like splice first arg is string, 2nd is starting index and 3rd is stopping index.
+    // echo substr ($phrase, 6, 2);
+    // echo "<br>";
+    // echo crypt($phrase);
+    // echo "<br>";
+
+    // Integers
+$num = 10;
+echo $num;
+$num++;
+echo "<br>";
+echo $num;
+echo "<br>";
+
+
+    echo 33;
+    echo "<br>";
+    echo -11;
+    echo "<br>";
+    echo 12.23;
+    echo "<br>";
+    echo 5/3;
+    echo "<br>";
+    echo 10%3;
+    echo "<br>";
+    echo (4 + 5) *10;
+    echo "<br>";
+
+
+    ?>
+</body>
+
+</html>
